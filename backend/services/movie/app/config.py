9b0777cf4daa97579fc17fb4c87d0322f79e25d1
Pavel Melnik <pan4kkpan4@gmail.com>
2026-05-13 01:43:51 +0200

    added models schemas and routers for movie entity

---
+++ backend/services/movie/app/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9092"
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+
+settings = Settings()
