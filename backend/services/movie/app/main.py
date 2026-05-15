398a7538d6a24e51fd8c979d17f8c868f1f19ad1
Pavel Melnik <pan4kkpan4@gmail.com>
2026-05-15 15:40:48 +0200

    added cors configuration for localhost:5173

--- backend/services/movie/app/main.py
+++ backend/services/movie/app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import genres, movies, theaters
 
+origins = [
+    "http://localhost:5173",
+]
+
 app = FastAPI(title="Movie Service", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(movies.router, prefix="/api/v1")
 app.include_router(genres.router, prefix="/api/v1")
